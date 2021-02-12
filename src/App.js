7c1d68afcbec5975cbe9e1ca1fffd6498361b38a
Ekaterina Bilenko <75737723+KatyaAms@users.noreply.github.com>
2021-02-12 15:27:50 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <div className="container">
      Weather app
      <footer>
        This project was coded by <a href="https://www.linkedin.com/in/ekaterinabilenko/" target="_blank">Ekaterina Bilenko</a> and is <a href="https://github.com/KatyaAms/Final-project-React" target="_blank">open-sourced on GitHub</a>
      </footer>
+    </div>
     </div>
   );
 }
