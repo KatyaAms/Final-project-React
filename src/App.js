ad18c51020fcf57dba6687ba52296e50bb3bdcee
Ekaterina Bilenko <75737723+KatyaAms@users.noreply.github.com>
2021-02-12 14:55:50 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
      Weather app
+     <footer>
+       This project was coded by <a href="https://www.linkedin.com/in/ekaterinabilenko/" target="_blank">Ekaterina Bilenko</a> and is <a href="https://github.com/KatyaAms/Final-project-React" target="_blank">open-sourced on GitHub</a>
+     </footer>
     </div>
   );
 }
