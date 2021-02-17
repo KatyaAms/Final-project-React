b10d4964b7e200b167a8dbfa885e85e470f059ab
Ekaterina Bilenko <75737723+KatyaAms@users.noreply.github.com>
2021-02-17 23:53:42 +0100

    Added some HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Weather from "./Weather";
+
 
 function App() {
   return (
     <div className="App">
     <div className="container">
      Weather app
+     <Weather />
      <footer>
        This project was coded by <a href="https://www.linkedin.com/in/ekaterinabilenko/" target="_blank">Ekaterina Bilenko</a> and is <a href="https://github.com/KatyaAms/Final-project-React" target="_blank">open-sourced on GitHub</a>
      </footer>
